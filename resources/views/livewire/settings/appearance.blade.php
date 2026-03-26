34ddd11d39903b16baf69eb6625e5e3307c5c170
Cfls <cflsapp@gmail.com>
2026-03-26 13:58:39 +0100

    fixed color light

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -1,5 +1,5 @@
 <section class="w-full flex items-center justify-center mx-auto px-4 py-8">
-    <div class="w-full max-w-2xl border-2 border-black dark:border-white rounded-lg p-4 bg-gray-700 dark:bg-gray-900">
+    <div class="w-full max-w-2xl border-2 border-black dark:border-white rounded-lg p-4  dark:bg-gray-900">
         @include('partials.settings-heading')
 
         <x-settings.layout :heading="__('Apparence')" :subheading=" __('Mettre à jour les paramètres d\'apparence de votre compte')">
