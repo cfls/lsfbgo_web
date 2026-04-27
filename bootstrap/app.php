d9c12bff7118d230b175b20c17f75b4529605341
Leonel Lopez <technoquo@gmail.com>
2026-04-27 14:39:46 +0200

    configurar cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,8 +5,6 @@ use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CheckNetworkConnection;
-
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'guest' => RedirectIfAuthenticated::class,
         ]);
 
-
+        $middleware->web(append: [
+            \Livewire\Features\SupportDisablingBackButtonCache\DisableBackButtonCacheMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
